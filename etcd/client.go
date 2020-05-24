c256ace9f25d8640c52fafd5051e13a4e1aca372
Elle Mouton <elle.mouton@gmail.com>
2020-05-24 17:28:27 +0200

    etcd: Client for etcd to make mocking easier

---
+++ etcd/client.go
@@ -0,0 +1,39 @@
+package etcd
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"time"
+
+	"go.etcd.io/etcd/clientv3"
+)
+
+var etcdHost = flag.String("etcd_host", "localhost:2379", "etcd host")
+var etcdUser = flag.String("etcd_user", "", "etcd user")
+var etcdPassword = flag.String("etcd_password", "", "etcd password")
+
+type client struct {
+	*clientv3.Client
+}
+
+type Client interface {
+	Close() error
+	Put(ctx context.Context, key, val string, opts ...clientv3.OpOption) (*clientv3.PutResponse, error)
+}
+
+func New() (Client, error) {
+	ec, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{*etcdHost},
+		DialTimeout: 5 * time.Second,
+		Username:    *etcdUser,
+		Password:    *etcdPassword,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("unable to connect to etcd: %v", err)
+	}
+
+	return &client{
+		ec,
+	}, nil
+}
